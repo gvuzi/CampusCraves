1b13532a58e698d924ca075deb262cf5a4b4d5d6
gvuzi <gideoncallagan@gmail.com>
2024-04-26 00:55:31 -0500

    added landing page

---
+++ app/src/main/java/edu/utsa/campuscraves/LandingPage.java
@@ -0,0 +1,35 @@
+package edu.utsa.campuscraves;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.ComponentActivity;
+import androidx.annotation.Nullable;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class LandingPage extends ComponentActivity{
+    private Button button;
+    AssetManager assets;
+
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.landing_page);
+            assets = getAssets();
+            setupButtons();
+        }
+
+        public void setupButtons() {
+            button = (Button) findViewById(R.id.Enter);
+        }
+}
+
+
+
+
