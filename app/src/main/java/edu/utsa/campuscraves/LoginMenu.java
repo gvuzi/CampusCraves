5a840e801f0cf49e14f770bda5ff4bcb1136808c
gvuzi <gideoncallagan@gmail.com>
2024-04-28 14:34:48 -0500

    added working LoginMenu and RegisterMenu validation

--- app/src/main/java/edu/utsa/campuscraves/LoginMenu.java
+++ app/src/main/java/edu/utsa/campuscraves/LoginMenu.java
@@ -5,8 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.ComponentActivity;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class LoginMenu extends ComponentActivity {
 
@@ -21,7 +27,22 @@ public class LoginMenu extends ComponentActivity {
         Button registerButton = (Button) findViewById(R.id.registerNewAccount);
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                EditText usernameInput = (EditText) findViewById(R.id.loginUsername);
+                EditText passwordInput = (EditText) findViewById(R.id.loginPassword);
+                int id = authenticate(usernameInput.getText().toString(), passwordInput.getText().toString());
+                if (id > 0) {
+                    Toast.makeText(getBaseContext(), "account is registered", Toast.LENGTH_SHORT).show();
+                    //Intent intent = new Intent(LoginMenu.this, ProfileActivity.class);
+                    //intent.putExtra("id", id);
+                    //startActivity(intent);
+                } else {
+                    usernameInput.setText("");
+                    passwordInput.setText("");
+                    usernameInput.setError("Incorrect username and password combination.");
+                    passwordInput.setError("Incorrect username and password combination.");
+                }
             }
+
         });
 
         registerButton.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +51,35 @@ public class LoginMenu extends ComponentActivity {
                     startActivity(intent);
             }
         });
+    }
+
+    private int authenticate(String username, String password) {
+        Scanner scan;
+        String str = "";
+        String[] userInfo = null;
+        boolean authenticated = false;
+        int id = -1;
+        File f = new File(getFilesDir().getAbsolutePath() + "/login.txt");
+
+        try {
+            if(f.exists()) {
+                scan = new Scanner(openFileInput("login.txt"));
+                while (scan.hasNext()) {
+                    str = scan.nextLine();
+                    userInfo = str.split(",");
+                    if (username.equalsIgnoreCase(userInfo[1]) && password.equals(userInfo[2])) {
+                        authenticated = true;
+                        id = Integer.parseInt(userInfo[0]);
+                        break;
+                    }
+                }
+                scan.close();
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return id;
     }
 }
--- app/src/main/java/edu/utsa/campuscraves/RegisterMenu.java
+++ app/src/main/java/edu/utsa/campuscraves/RegisterMenu.java
@@ -4,8 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.ComponentActivity;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class RegisterMenu extends ComponentActivity {
     public void onCreate(Bundle savedInstanceState) {
@@ -19,13 +26,128 @@ public class RegisterMenu extends ComponentActivity {
         Button signInButton = (Button) findViewById(R.id.signInButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                int id = -1;
+                EditText firstNameInput = (EditText) findViewById(R.id.registerFirstName);
+                EditText lastNameInput = (EditText) findViewById(R.id.registerLastName);
+                EditText usernameInput = (EditText) findViewById(R.id.registerUsername);
+                EditText passwordInput = (EditText) findViewById(R.id.registerPassword);
+
+                if (isInputEmpty()) {
+                    id = createLogin();
+                    if (id > 0) {
+                        createAccount(id);
+                    }
+                    finish();
+                }
+                else {
+                    firstNameInput.setText("");
+                    lastNameInput.setText("");
+                    usernameInput.setText("");
+                    passwordInput.setText("");
+
+                    firstNameInput.setError("All fields must be filled out.");
+                    lastNameInput.setError("All fields must be filled out.");
+                    usernameInput.setError("All fields must be filled out.");
+                    passwordInput.setError("All fields must be filled out.");
+                }
             }
         });
+
+
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                finish();
             }
         });
+    }
+
+    private boolean isInputEmpty() {
+        EditText firstNameInput = (EditText) findViewById(R.id.registerFirstName);
+        EditText lastNameInput = (EditText) findViewById(R.id.registerLastName);
+        EditText usernameInput = (EditText) findViewById(R.id.registerUsername);
+        EditText passwordInput = (EditText) findViewById(R.id.registerPassword);
+
+        return !firstNameInput.getText().toString().isEmpty() && !lastNameInput.getText().toString().isEmpty() &&
+                !usernameInput.getText().toString().isEmpty() && !passwordInput.getText().toString().isEmpty();
+    }
+
+    private int createLogin() {
+        EditText usernameInput = (EditText) findViewById(R.id.registerUsername);
+        EditText passwordInput = (EditText) findViewById(R.id.registerPassword);
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        File f = new File(getFilesDir().getAbsolutePath() + "/login.txt");
+        OutputStreamWriter w = null;
+        Scanner scan;
+        int id = -1;
+        String str = null;
+        String[] arr;
+
+        if (!f.exists()) {
+            id = 1;
+            try {
+                w = new OutputStreamWriter(openFileOutput("login.txt", MODE_PRIVATE));
+                w.write(id + "," + username + "," + password);
+                w.close();
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "IOException: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            try {
+                scan = new Scanner(openFileInput("login.txt"));
+                while (scan.hasNextLine()) {
+                    str = scan.nextLine();
+                }
+                if (str != null) {
+                    arr = str.split(",");
+                    if (arr.length == 3) {
+                        id = Integer.parseInt(arr[0]) + 1;
+                    }
+                }
+                scan.close();
+
+                w = new OutputStreamWriter(openFileOutput("login.txt", MODE_APPEND));
+                w.append("\n" + id + "," + username + "," + password);
+                w.close();
+
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "IOException: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        return id;
+    }
+
+    private void createAccount(int id) {
+        EditText firstNameInput = (EditText) findViewById(R.id.registerFirstName);
+        EditText lastNameInput = (EditText) findViewById(R.id.registerLastName);
+        String firstName = firstNameInput.getText().toString();
+        String lastName = lastNameInput.getText().toString();
+
+        File f = new File(getFilesDir().getAbsolutePath() + "/accounts.txt");
+        OutputStreamWriter w = null;
+
+        if (!f.exists()) {
+            try {
+                w = new OutputStreamWriter(openFileOutput("accounts.txt", MODE_PRIVATE));
+                w.write(id + "," + firstName + "," + lastName);
+                w.close();
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "IOException: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            try {
+                w = new OutputStreamWriter(openFileOutput("accounts.txt", MODE_APPEND));
+                w.append("\n" + id + "," + firstName + "," + lastName);
+                w.close();
+            }
+            catch(IOException e) {
+                Toast.makeText(getBaseContext(), "IOException: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 }
